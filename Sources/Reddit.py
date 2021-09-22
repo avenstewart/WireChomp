662f07b747ff95ca4de5d68486129553491d81e5
avenstewart <avenstewart@gmail.com>
2021-09-22 01:20:49 -0500

    Reddit scraping progress and HTML content extraction added. Pushshift API call requires adjustment

--- Sources/Reddit.py
+++ Sources/Reddit.py
@@ -1,48 +1,72 @@
 import configparser
 import praw
-from requests import Session
+import json
+from time import sleep
+from requests import Session, get
 
 class RedditChomper:
 
-    config = configparser.ConfigParser()
-    config.read('resources/sourceconfig.ini')
-    reddit_config = config['REDDIT']
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('resources/sourceconfig.ini')
+        self.reddit_config = config['REDDIT']
 
-    def init_session(self):
-
-        session = Session()
-        session.verify = self.reddit_config['certfile_path']
-        return praw.Reddit(
-            client_id="",
-            client_secret="",
-            password="",
-            requestor_kwargs={"session": session},
-            user_agent="",
-            username=""
+        self.session = Session()
+        self.session.verify = self.reddit_config['certfile_path']
+        self.reddit = praw.Reddit(
+            client_id=self.reddit_config['client_id'],
+            client_secret=self.reddit_config['client_secret'],
+            password=self.reddit_config['password'],
+            requestor_kwargs={"session": self.session},
+            user_agent=self.reddit_config['user_agent'],
+            username=self.reddit_config['username']
         )
 
-    @staticmethod
-    def get_before_date(datetime, max_articles):
-        # get a list of all (up tp max_articles) article objects -
-        # returned in a 24 hours window before supplied datetime
-        config = configparser.ConfigParser()
-        config.read('sourceconfig.ini')
-        sub_urls = config['REDDIT']['subreddit_list']
+    # get a list of all (up tp max_articles) article objects -
+    # returned in a 24 hours window before supplied datetime
+    def get_posts_before_datetime(self, date_millis, lookback_hours, max_results):
+        results = []
+        elapsed_time = 3600000*lookback_hours
+        after_timestamp = date_millis - elapsed_time
+        subslist = self.reddit_config['subreddit_list']
+        sub_urls = json.loads(subslist)
 
         for sub in sub_urls:
-            subreddit =  praw.reddit.Subreddit(sub)
-            for submission in praw
+            pushshift_url = (
+                "https://api.pushshift.io/reddit/search/submission?subreddit=%s&after=%d&before=%d&size=%d"
+                % (sub, after_timestamp, date_millis, max_results)
+            )
+            bulk_articles_json = get(pushshift_url).json()
+            subreddit = self.reddit.subreddit(sub)
+            for post_obj in bulk_articles_json['data']:
+                results.append(post_obj)
+            sleep(int(self.reddit_config['request_delay']))
+        return results
 
     @staticmethod
-    def get_raw_links_from_sub(sub, date, limit):
-        sub_url = "https://reddit.com/r/%s" % sub
+    def filter_by_score(posts_json, score_thresh):
+        results = []
+        for post in posts_json:
+            if post['score'] >= score_thresh:
+                results.append(post)
+        return results
 
+    @staticmethod
+    def filter_by_keywords(posts_json, keywords=list):
+        results = []
+        for post in posts_json:
+            # noinspection PyTypeChecker
+            if all(word in post['title'] for word in keywords):
+                results.append(post)
+        return results
 
-    def map_article_to_obj(self, raw):
-        # map each articles datetime, title, and primary content to an ArticleMap object
-        # assign a popularity weight to the article based on upvotes
+    @staticmethod
+    def filter_by_news_source(posts_json, exclusive=bool, hostnames=list):
+        results = []
+        for post in posts_json:
+            # noinspection PyTypeChecker
+            if any(hostname in post['domain'] for hostname in hostnames):
+                results.append(post)
+        return results
 
-    def content_from_raw(self):
-        # retrieve the raw article and determine the type of source
-        # grab the main content with the corresponding extraction method for that specific news source
 
